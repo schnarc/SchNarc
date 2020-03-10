mask for dyson norm learning fixed bug to train only on energies and not only gradients training for dyson norm and dublets/quartets now possible

src/schnarc/model.py
@@ -85,7 +85,9 @@ class MultiStatePropertyModel(nn.Module):
         self.n_in = n_in
         self.n_states = n_states
         self.n_singlets = n_states['n_singlets']
+        self.n_dublets  = n_states['n_dublets']
         self.n_triplets = n_states['n_triplets']
+        self.n_quartets = n_states['n_quartets']
         self.nmstates = n_states['n_states']
         self.properties = properties
         self.need_atomic = need_atomic
@@ -96,6 +98,10 @@ class MultiStatePropertyModel(nn.Module):
         self.inverse_energy = inverse_energy
 
         self.derivative = self.need_forces
+        if self.derivative:
+            create_graph=True
+        else:
+            create_graph=False
         # Construct default mean and stddevs if passed None
         if mean is None or stddev is None:
             mean = {p: None for p in properties}
@@ -112,18 +118,18 @@ class MultiStatePropertyModel(nn.Module):
             except:
                 atomref = None
 
-            energy_module = MultiEnergy(n_in, self.n_singlets + self.n_triplets, aggregation_mode='sum',
+            energy_module = MultiEnergy(n_in, self.n_singlets + self.n_dublets + self.n_triplets + self.n_quartets, aggregation_mode='sum',
                                         return_force=self.need_forces,
                                         return_contributions=self.need_atomic, mean=mean[Properties.energy],
                                         stddev=stddev[Properties.energy],
-                                        atomref=atomref, create_graph=True, n_layers=n_layers)
+                                        atomref=atomref, create_graph=create_graph, n_layers=n_layers)
 
             outputs[Properties.energy] = energy_module
 
         # Dipole moments and transition dipole moments
         if self.need_dipole:
-            n_dipoles = int((self.n_singlets+self.n_triplets) * (self.n_singlets+self.n_triplets + 1) / 2)  # Between ALL states
-            dipole_module = MultiDipole(n_in, n_states, n_layers=n_layers)
+            n_dipoles = int((self.n_singlets*(self.n_singlets+1)/2) + (self.n_dublets*(self.n_dublets+1)/2) +(self.n_triplets*(self.n_triplets+1)/2) * (self.n_quartets*(self.n_quartets + 1) / 2))  # between each states
+            dipole_module = MultiDipole(n_in, n_dipoles, n_layers=n_layers)
             outputs[Properties.dipole_moment] = dipole_module
 
         # Nonadiabatic couplings
@@ -135,10 +141,16 @@ class MultiStatePropertyModel(nn.Module):
         # Spinorbit couplings
         if self.need_socs:
             n_socs = int(
-                (self.n_singlets+3*self.n_triplets) * (self.n_singlets+3*self.n_triplets-1))  # Between all different states - including imaginary numbers
+                (self.n_singlets+2*self.n_dublets+3*self.n_triplets+4*self.n_quartets) * (self.n_singlets+2*self.n_dublets+3*self.n_triplets+4*self.n_quartets-1))  # Between all different states - including imaginary numbers
             socs_module = MultiSoc(n_in, n_socs, n_layers=n_layers, real=real, mean=mean[Properties.socs],
                                    stddev=stddev[Properties.socs])
             outputs[Properties.socs] = socs_module
+
+
+        if self.need_dyson:
+            n_dyson = self.n_singlets*(self.n_dublets+self.n_quartets)+self.n_triplets*(self.n_dublets+self.n_quartets)
+            dyson_module = MultiDyson(n_in, n_dyson, n_layers=n_layers)
+            outputs[Properties.dyson] = dyson_module
 
         self.output_dict = nn.ModuleDict(outputs)
 
@@ -148,6 +160,7 @@ class MultiStatePropertyModel(nn.Module):
         self.need_dipole = Properties.dipole_moment in properties
         self.need_nacs = Properties.nacs in properties
         self.need_socs = Properties.socs in properties
+        self.need_dyson = Properties.dyson in properties
 
     def forward(self, inputs):
 
@@ -185,7 +198,7 @@ class MultiState(Atomwise):
     def __init__(self, n_in, n_states, aggregation_mode='sum', n_layers=2, n_neurons=None,
                  activation=spk.nn.activations.shifted_softplus, return_contributions=False, create_graph=True,
                  return_force=False, mean=None, stddev=None, atomref=None, max_z=100, outnet=None,
-                 standardize_after=False, return_hessian=[False,1,0.018,1,0.036]):
+                 standardize_after=False, return_hessian=[False,1,0.018,1,0.036,1,0.018,1,0.036]):
 
         self.n_states = n_states
         self.return_hessian = return_hessian
@@ -246,9 +259,13 @@ class MultiState(Atomwise):
             result['dydx'] = dydr
             if self.return_hessian[0]:
                 n_singlets=self.return_hessian[1]
-                n_triplets=self.return_hessian[3]
+                n_triplets=self.return_hessian[5]
+                n_dublets = self.return_hessian[3]
+                n_quartets = self.return_hessian[7]
                 threshold_dE_S=self.return_hessian[2]
-                threshold_dE_T=self.return_hessian[4]
+                threshold_dE_D= self.return_hessian[4]
+                threshold_dE_T=self.return_hessian[6]
+                threshold_dE_Q=self.return_hessian[8]
                 compute_hessian=False
                 for istate in range(n_singlets):
                     for jstate in range(istate+1,n_singlets):
@@ -256,9 +273,17 @@ class MultiState(Atomwise):
                             compute_hessian=True
                         else:
                             pass
-                for istate in range(n_singlets,n_singlets+n_triplets):
-                    for jstate in range(istate+1,n_singlets+n_triplets):
+                for istate in range(n_singlets,n_singlets+n_dublets):
+                    for jstate in range(istate+1,n_singlets+n_dublets):
+                        if abs(result['y'][0][istate].item()-result['y'][0][jstate].item()) <= threshold_dE_D:
+                            compute_hessian=True
+                for istate in range(n_singlets+n_dublets,n_singlets+n_dublets+n_triplets):
+                    for jstate in range(istate+1,n_singlets+n_dublets+n_triplets):
                         if abs(result['y'][0][istate].item()-result['y'][0][jstate].item()) <= threshold_dE_T:
+                            compute_hessian=True
+                for istate in range(n_singlets+n_dublets+n_triplets,n_singlets+n_dublets+n_triplets+n_quartets):
+                    for jstate in range(istate+1,n_singlets+n_dublets+n_triplets+n_quartets):
+                        if abs(result['y'][0][istate].item()-result['y'][0][jstate].item()) <= threshold_dE_Q:
                             compute_hessian=True
                         else:
                             pass
@@ -299,9 +324,9 @@ class MultiEnergy(MultiState):
     def __init__(self, n_in, n_states, aggregation_mode='sum', n_layers=2,
                  n_neurons=None,
                  activation=spk.nn.activations.shifted_softplus,
-                 return_contributions=False, create_graph=True,
+                 return_contributions=False, create_graph=False,
                  return_force=False, mean=None, stddev=None, atomref=None,
-                 max_z=100, outnet=None, return_hessian=[False,1,0,1,0]):
+                 max_z=100, outnet=None, return_hessian=[False,1,0,1,0,1,0,1,0]):
         super(MultiEnergy, self).__init__(n_in, n_states, aggregation_mode, n_layers,
                                           n_neurons, activation,
                                           return_contributions, return_force,
@@ -354,13 +379,9 @@ class MultiDipole(MultiState):
             If requires_dr is true additionally returns forces
         """
 
-    def __init__(self, n_in, n_states, aggregation_mode='sum', n_layers=2, n_neurons=None,
-                 activation=spk.nn.activations.shifted_softplus, create_graph=True, outnet=None):
+    def __init__(self, n_in, n_couplings, aggregation_mode='sum', n_layers=2, n_neurons=None,
+                 activation=spk.nn.activations.shifted_softplus, create_graph=False, outnet=None):
 
-        n_singlets = n_states[Properties.n_singlets]
-        n_triplets = n_states[Properties.n_triplets]
-        nmstates   = n_singlets + 3 * n_triplets
-        n_couplings = int(nmstates * (nmstates - 1) / 2 + nmstates)
 
         super(MultiDipole, self).__init__(n_in, n_couplings,
                                           aggregation_mode=aggregation_mode,
@@ -415,7 +436,7 @@ class MultiNac(MultiState):
             If requires_dr is true additionally returns forces
         """
     def __init__(self, n_in, n_states, aggregation_mode='sum', n_layers=2, n_neurons=None,
-                 activation=spk.nn.activations.shifted_softplus, return_contributions=False, create_graph=True,
+                 activation=spk.nn.activations.shifted_softplus, return_contributions=False, create_graph=False,
                  outnet=None, use_inverse=False):
 
         n_singlets = n_states[Properties.n_singlets]
@@ -441,7 +462,7 @@ class MultiNac(MultiState):
             self.approximate_inverse = schnarc.nn.ApproximateInverse(reconsts,n_triplets=n_triplets)
 
     def forward(self, inputs):
-        self.return_hessian=[False,1,0,1,0]
+        self.return_hessian=[False,1,0,1,0,1,0,1,0]
         result = super(MultiNac, self).forward(inputs)
         result['y2']=result['y']
         if 'nac_energy' in inputs and self.use_inverse:
@@ -507,6 +528,56 @@ class MultiSoc(MultiState):
 
         return result
 
+class MultiDyson(MultiState):
+    """
+        Predicts property matrix. Only contains real values, no phase correction needed.
+
+        Args:
+            n_in (int): input dimension of representation
+            pool_mode (str): one of {sum, avg} (default: sum)
+            n_layers (int): number of nn in output network (default: 2)
+            n_neurons (list of int or None): number of neurons in each layer of the output network.
+                                              If `None`, divide neurons by 2 in each layer. (default: none)
+            activation (function): activation function for hidden nn (default: spk.nn.activations.shifted_softplus)
+            return_contributions (bool): If True, latent atomic contributions are returned as well (default: False)
+            outnet (callable): network used for atomistic outputs. Takes schnetpack input dictionary as input. Output is
+                               not normalized. If set to None (default), a pyramidal network is generated automatically.
+
+        Returns:
+            Prediction of single values that will be parsed to the property matrix for SchNarc
+
+        """
+
+    def __init__(self, n_in, n_states, aggregation_mode='sum', n_layers=2, n_neurons=None,
+                 activation=spk.nn.activations.shifted_softplus, create_graph=False, mean=None, stddev=None,
+                 outnet=None):
+        super(MultiDyson, self).__init__(n_in, n_states,
+                                       aggregation_mode=aggregation_mode,
+                                       n_layers=n_layers,
+                                       n_neurons=n_neurons,
+                                       activation=activation,
+                                       return_contributions=False,
+                                       create_graph=create_graph,
+                                       mean=mean,
+                                       stddev=stddev,
+                                       atomref=None,
+                                       max_z=100,
+                                       outnet=outnet)
+
+        self.output_mask = schnarc.nn.DysonTransform(n_states)
+
+    def forward(self, inputs):
+        """
+        predicts spin-orbit couplings
+        """
+
+        result = super(MultiDyson, self).forward(inputs)
+        dyson = self.output_mask(result['y'])
+        result['y'] = dyson
+
+        return result
+
+
 
 class HiddenStatesEnergy(Atomwise):
     """
@@ -516,7 +587,7 @@ class HiddenStatesEnergy(Atomwise):
     def __init__(self, n_in, n_states, aggregation_mode='sum', n_layers=2,
                  n_neurons=None,
                  activation=spk.nn.activations.shifted_softplus,
-                 return_contributions=False, create_graph=True,
+                 return_contributions=False, create_graph=False,
                  return_force=False, mean=None, stddev=None, atomref=None,
                  max_z=100, outnet=None):
         self.n_states = n_states

src/scripts/run_schnarc.py
@@ -81,7 +81,7 @@ def get_parser():
     pred_parser.add_argument('datapath', help='Path / destination of MD17 dataset directory')
     pred_parser.add_argument('modelpath', help='Path of stored model')
     pred_parser.add_argument('--hessian', action='store_true', help='Gives back the hessian of the PES.')
-    pred_parser.add_argument('--nac_approx',type=float, nargs=3, default=[1,0.018,0.036],help='Type of NAC approximation as first value and threshold for energy gap in Hartree as second value.')
+    pred_parser.add_argument('--nac_approx',type=float, nargs=5, default=[1,0.018,0.036,0.018,0.036],help='Type of NAC approximation as first value and threshold for energy gap in Hartree as second value.')
     # model-specific parsers
     model_parser = argparse.ArgumentParser(add_help=False)
 
@@ -246,7 +246,7 @@ def train(args, model, tradeoffs, train_loader, val_loader, device, n_states, pr
                 else:
                     prop_err = torch.mean(prop_diff.view(-1))
             else:
-                if prop=='energy' and result['forces'].shape[1]==int(1) or prop=='forces' and result['forces'].shape[1]==int(1) or prop=='dipoles' and result['forces'].shape[1]==int(1):
+                if result['energy'].shape[1]==int(1):
                     prop_diff = batch[prop][:,0] - result[prop][:,0]
                 else:
                     prop_diff = batch[prop] - result[prop]
@@ -474,12 +474,12 @@ if __name__ == '__main__':
     if args.mode != 'train':
         model = torch.load(os.path.join(args.modelpath, 'best_model'), map_location='cpu').to(device)
         if args.hessian == True:
-            model.output_modules.output_dict['energy'].return_hessian = [True,1,1,1,1]
+            model.output_modules.output_dict['energy'].return_hessian = [True,1,1,1,1,1,1,1,1]
         else:
-            model.output_modules.output_dict['energy'].return_hessian = [False,1,1]
+            model.output_modules.output_dict['energy'].return_hessian = [False,1,1,1,1]
  
     if args.mode == 'pred':
-        pred_data = spk.data.AtomsData(args.datapath, required_properties=[])
+        pred_data = spk.data.AtomsData(args.datapath)
         pred_loader = spk.data.AtomsLoader(pred_data, batch_size=args.batch_size, num_workers=2, pin_memory=True)
         run_prediction(model, pred_loader, device, args)
         sys.exit(0)
@@ -522,25 +522,17 @@ if __name__ == '__main__':
     dataset = spk.data.AtomsData(args.datapath, collect_triples=args.model == 'wacsf')
     # Determine the number of states based on the metadata
     n_states = {}
-    n_states['n_singlets'] = dataset.get_metadata("n_singlets")
-    if dataset.get_metadata("n_triplets") == None:
-        n_states['n_triplets']=int(0)
-    else:
-        n_states['n_triplets'] = dataset.get_metadata("n_triplets")
-    n_states['n_states'] = n_states['n_singlets'] + n_states['n_triplets']
-
-    ##activate if only one state is learned or not all
+    #activate if only one state is learned or not all
     s=tradeoffs['energy'].split()
-    if int(s[1]) > int(0):
-        n_singlets = int(s[1])
-        n_dublets  = int(s[2])
-        n_triplets = int(s[3])
-        n_quartets = int(s[4])
-        n_states['n_singlets'] = n_singlets
-        n_states['n_dublets'] = n_dublets
-        n_states['n_triplets'] = n_triplets
-        n_states['n_quartets'] = n_quartets
-        n_states['n_states'] = n_states['n_singlets'] + +n_states['n_dublets']+n_states['n_triplets']+n_states['n_quartets']
+    n_singlets = int(s[1])
+    n_dublets  = int(s[2])
+    n_triplets = int(s[3])
+    n_quartets = int(s[4])
+    n_states['n_singlets'] = n_singlets
+    n_states['n_dublets'] = n_dublets
+    n_states['n_triplets'] = n_triplets
+    n_states['n_quartets'] = n_quartets
+    n_states['n_states'] = n_states['n_singlets'] + +n_states['n_dublets']+n_states['n_triplets']+n_states['n_quartets']
     n_states['states'] = dataset.get_metadata("states")
     logging.info('Found {:d} states... {:d} singlet states, {:d} dublet states, {:d} triplet states, and {:d} quartet states'.format(n_states['n_states'],
                                                                                            n_states['n_singlets'],
@@ -616,11 +608,12 @@ if __name__ == '__main__':
         #n_nacs = int(n_states['n_singlets']*(n_states['n_singlets']-1)/2 + n_states['n_triplets']*(n_states['n_triplets']-1)/2 )
         batch_size = args.batch_size
         all_states = n_states['n_singlets'] + 2 * n_states['n_dublets'] + 3 * n_states['n_triplets'] + 4 * n_states['n_quartets']
+        n_states['n_states_st'] = n_states['n_singlets'] + n_states['n_triplets']
         n_socs = int(all_states * (all_states - 1)) # complex so no division by zero
         #min loss for a given batch size
         #vector with correct phases for a mini batch
         #number of possible phase vectors
-        n_phases = int(2**(n_states['n_states']-1))
+        n_phases_st = int(2**(n_states['n_states_st']-1))
         #number of singlet-singlet and triplet-triplet deriative couplings
         #gives the number of phases 
         #generate all possible 2**(nstates-1) vectors that give rise to possible combinations of phases
@@ -629,11 +622,11 @@ if __name__ == '__main__':
         this should be separated and exactly the same as for singlets and triplets
         the socs should then be separated in order to get the right phases with respect to the phases of nacs between dublets-dublets and quartets-quartets
         in case only 1 property, such as only socs, are computed: phaseless_loss_single can be applied and this is not necessary"""
-        phasevector = generateAllBinaryStrings(n_states['n_states'],[None]*n_states['n_states'],0,[])
-        phase_pytorch = torch.Tensor( n_states['n_states'],n_phases ).to(device)
+        phasevector = generateAllBinaryStrings(n_states['n_states_st'],[None]*n_states['n_states_st'],0,[])
+        phase_pytorch = torch.Tensor( n_states['n_states_st'],n_phases_st ).to(device)
         iterator = -1
-        for i in range(n_phases):
-            for j in range(n_states['n_states']):
+        for i in range(n_phases_st):
+            for j in range(n_states['n_states_st']):
                 iterator+=1
                 if phasevector[iterator]==0:
                     phase_pytorch[j,i] = 1
@@ -642,7 +635,7 @@ if __name__ == '__main__':
 
         socs_phase_matrix_1, socs_phase_matrix_2, diag_phase_matrix_1, diag_phase_matrix_2 = generateAllPhaseMatrices(phase_pytorch,n_states,n_socs,all_states,device)
 
-        props_phase=[n_phases,batch_size,device,phase_pytorch,n_socs, all_states, socs_phase_matrix_1, socs_phase_matrix_2, diag_phase_matrix_1, diag_phase_matrix_2]
+        props_phase=[n_phases_st,batch_size,device,phase_pytorch,n_socs, all_states, socs_phase_matrix_1, socs_phase_matrix_2, diag_phase_matrix_1, diag_phase_matrix_2]
         train(args, model, tradeoffs, train_loader, val_loader, device, n_states,props_phase)
         logging.info("...training done!")
 