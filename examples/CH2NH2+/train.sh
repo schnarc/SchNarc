1eaaa4f38aa665ab01dd69457a7e4770e370156f
Julia Westermayr <julia@dubnium.itc.univie.ac.at>
2020-04-22 16:00:34 +0200

    added additional files for testing

--- examples/CH2NH2+/train.sh
+++ examples/CH2NH2+/train.sh
@@ -1,2 +1,2 @@
-python $SCHNARC/run_schnarc.py train schnet --split 100 10 --batch_size 10 --min_loss --tradeoffs tradeoffs.yaml --inverse_nacs --overwrite $SCHNARC/../examples/CH2NH2+/CH2NH2+.db TEST
+python $SCHNARC/run_schnarc.py train schnet --split 100 10 --batch_size 10 --tradeoffs tradeoffs.yaml $SCHNARC/../examples/CH2NH2+/CH2NH2+.db TEST
 
